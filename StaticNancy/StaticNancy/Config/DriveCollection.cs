fd4f72a3a6f969a625fa1d3558a49acd4aec4f08
adarmus <adam@blairfamily.co.uk>
2018-01-18 17:15:56 +0000

    Added config classes

---
+++ StaticNancy/StaticNancy/Config/DriveCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticNancy.Config
+{
+    class DriveCollection : ConfigurationElementCollection, IEnumerable<DriveElement>
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new DriveElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((DriveElement)element).Drive;
+        }
+
+        public new IEnumerator<DriveElement> GetEnumerator()
+        {
+            int count = base.Count;
+            for (int i = 0; i < count; i++)
+            {
+                yield return base.BaseGet(i) as DriveElement;
+            }
+        }
+    }
+}
---
+++ StaticNancy/StaticNancy/Config/DriveElement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticNancy.Config
+{
+    class DriveElement : ConfigurationElement
+    {
+        [ConfigurationProperty("drive", IsRequired = true)]
+        public string Drive
+        {
+            get { return (string)this["drive"]; }
+            set { this["drive"] = value; }
+        }
+
+        [ConfigurationProperty("drivePwd", IsRequired = true)]
+        public string DrivePwd
+        {
+            get { return (string)this["drivePwd"]; }
+            set { this["drivePwd"] = value; }
+        }
+
+        [ConfigurationProperty("driveMountCommand", IsRequired = true)]
+        public string DriveMountCommand
+        {
+            get { return (string)this["driveMountCommand"]; }
+            set { this["driveMountCommand"] = value; }
+        }
+
+        [ConfigurationProperty("driveUnmountCommand", IsRequired = true)]
+        public string DriveUnmountCommand
+        {
+            get { return (string)this["driveUnmountCommand"]; }
+            set { this["driveUnmountCommand"] = value; }
+        }
+    }
+}
